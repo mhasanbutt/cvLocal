0c66dc4a85d9a5fee63c755ee7c24122501c5b18
Muhammad Hassan <mhasanbutt@hotmail.com>
2023-07-20 19:13:24 +0500

    LoginPage

--- index.php
+++ index.php
@@ -4,49 +4,53 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
-    <link href="images/favicon.png" rel="shortcut icon"><title>CareVision - Less Admin, More Caring</title>
-    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
+    <link href="images/favicon.png" rel="shortcut icon">
+    <title>CareVision - Less Admin, More Caring</title>
+    <link href="css/bootstrap.min.css" rel="stylesheet">
     <link href="css/login.css" rel="stylesheet">
 </head>
 <body>
     <section class="login">
         <article>
             <form id="loginform" role="form" method="POST">
-                <img src="images/loginPage-logo.png" alt="CareVision" class="logo">
-                <input type="hidden" name="_token" value="1KQ2MfrOX3c6cy2S2jClSgY6EsgkjtrCMgxLaDUH">
-                <input id="email" type="email" name="email" placeholder="Email Address" value="" required="" autofocus="">
-                <input id="password" type="password" name="password" placeholder="Password" required="">
-                <div class="select unit">
-                    <select>
-                        <option>Unit</option>
+                <img src="images/logo.webp" alt="CareVision" class="logo">
+
+                <input id="username" type="text" name="usrname" placeholder="User Name" value="" required="" autofocus="" tabindex="1">
+
+                <input id="password" type="password" name="password" placeholder="Password" required="" tabindex="2">
+                <i></i>
+
+                <div class="select">
+                    <select tabindex="3">
+                        <option>Select Home</option>
                     </select>
-                    <img src="images/left-arrow.svg" class="svg">
                 </div>
-                <div class="select floor">
-                    <select>
-                        <option>Floor</option>
+
+                <div class="select">
+                    <select tabindex="4">
+                        <option>Select Floor</option>
                     </select>
-                    <img src="images/left-arrow.svg" class="svg">
                 </div>
-                <div class="clearfix text-left">
-                    <div class="rememberMe clearfix">
-                        <span class="toggles">
-                            <input type="checkbox" name="remember" id="remember" class="ios-toggle" checked>
-                            <label for="remember" class="checkbox-label" data-off="NO" data-on="YES"></label>
-                        </span>
-                        <label>Remember Me</label>
-                    </div>
-                    <button type="submit" class="pull-right"><img src="images/padlock.svg" class="svg">Sign In</button>
+
+                <div class="select">
+                    <select tabindex="5">
+                        <option>Keep Me Logged In</option>
+                        <option>For 15 minutes</option>
+                        <option>For 30 minutes</option>
+                        <option>For 45 minutes</option>
+                        <option>For 60 minutes</option>
+                        <option>For 2 hours</option>
+                        <option>For 12 hours</option>
+                    </select>
                 </div>
-                <hr>
-                <a href="http://localhost/laravel/public/password/reset">Forgot Your Password?</a>
+
+                <button type="submit" class="pull-right" tabindex="6">Log In</button>
             </form>
         </article>
     </section>
-    <span class="loginCopy">&copy; <?php echo date('Y');?> CareVision Management Ltd. All Rights Reserved.</span>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-    <script src="js/jquery.scrollbar.min.js"></script>
+
+    <script src="js/jquery-1.12.4.js"></script>
+    <script src="js/bootstrap.min.js"></script>
     <script src="js/custom.js"></script>
 </body>
 </html>
